fc85fe26b750ec9d698e6f5fe58c646b1132fafb
nikita <your@email.com>
2025-12-28 16:32:27 +0300

    the first commit vs

---
+++ code.py
@@ -0,0 +1,11 @@
+price = float(input("Введите цену товара: "))
+discount = float(input("Введите процент скидки: "))
+
+# Вычисляем скидку в рублях
+discount_amount = price * discount / 100
+
+# Вычисляем итоговую цену
+final_price = price - discount_amount
+
+# Выводим результат
+print(f"Цена со скидкой: {final_price} руб.")
